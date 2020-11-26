f8ea2b1c99507b7957f8b9866f3f0a2c434358b1
Roman Kocherezhchenko <romakoch2018@gmail.com>
2020-11-26 09:18:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// function sayHelloTo(name) {
+//  let sign = '?!'
+//  const message = 'Hello' + ' ' + name
+//  return (sign) => message + sign
+// }
+// let name = 'Roman'
+// say = sayHelloTo(name)
+// console.log(say('!')) // Hello Roman!
+// name = 'Oleg'
+// say2 = sayHelloTo(name)
+// console.log(say2('?')) // Hello Oleg?
+
+function createFrameworkManager() {
+  const fw = []
+
+  return {
+    print() {
+      console.log(fw.join(', '))
+    },
+    add(name) {
+      fw.push(name)
+    }
+  }
+}
+
+// frameworks = createFrameworkManager()
+// frameworks.print()
+// frameworks.add('Vue')
+// frameworks.add('React')
+// frameworks.print()
+
+const fib = [1,2,3,5,8,13]
+
+const log = (iterator, time) => {
+  return setTimeout(() => console.log(iterator), time)
+} 
+
+for (let i = 0; i < fib.length; i++) {
+  log(fib[i], 1000 * i)
+}
